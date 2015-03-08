bf5c30843f1c5e4f7c4bcb0f32906e32a7acc743
Kieran Raftery <kraftery7@gmail.com>
2015-03-07 21:10:25 -0500

    UISegmentedController works, but I still need to add the map

--- CommunityBoard/MainViewController.swift
+++ CommunityBoard/MainViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 
 class MainViewController: UIViewController {
     @IBOutlet var mode_control: UISegmentedControl!
-
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.mode_control.tintColor = UIColor(red: 211.0/255, green: 70.0/255, blue: 74.0/255, alpha: 1);
@@ -21,6 +22,15 @@ class MainViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func segmentChanged(sender: UISegmentedControl) {
+        
+        if mode_control.selectedSegmentIndex == 0 {
+            tableView.hidden = false
+        }
+        else {
+            tableView.hidden = true
+        }
+    }
     /*
     // MARK: - Navigation
 
